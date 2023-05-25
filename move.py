a6d1ae0abd991d7b3c66b3cce270909cb94e9f88
WorldTeacher <41587052+WorldTeacher@users.noreply.github.com>
2023-05-25 12:01:00 +0200

    new window management class

--- move.py
+++ move.py
@@ -3,12 +3,42 @@ import win32gui
 import regex as re
 #pyautogui.moveTo(1920,1080, duration=0.25)
 
+import win32gui
+import re
+
+
+class WindowMgr:
+    """Encapsulates some calls to the winapi for window management"""
+
+    def __init__ (self):
+        """Constructor"""
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """find a window by its class_name"""
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """Pass to win32gui.EnumWindows() to check all the opened windows"""
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """find a window whose title matches the wildcard regex"""
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetForegroundWindow(self._handle)
+
 
 def change_window(window_name):
     try:
         window_titles = pyautogui.getAllTitles()
-        # print(window_titles)
+        #print(window_titles)
         for title in window_titles:
+            #print(title)
             if window_name in title:
                 
                 window_pid = win32gui.FindWindowEx(None, None, None, title)
@@ -18,7 +48,21 @@ def change_window(window_name):
     except Exception as e:
         print(e)    
         raise Exception("Error occured, aborting")
-    
+   
+def change_focus(window):
+    try:
+        window_pid = win32gui.FindWindowEx(None, None, None, window)
+        #set focus
+        win32gui.SetForegroundWindow(window_pid)
+    except Exception as e:
+        print(e)    
+        raise Exception("Error occured, aborting") 
+
+
+
     
 if __name__ == '__main__':
-    change_window("PHFR: Katalog")
+    w = WindowMgr()
+    w.find_window_wildcard(".*PHFR:.*")
+    w.set_foreground()
+    # change_window("PHFR: Katalog")
